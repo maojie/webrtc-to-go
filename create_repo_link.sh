87ceff59dfdc3e64a5502ff6d919c01191eb43a9
maojie <maojie0924@gmail.com>
2019-03-27 18:58:32 +0800

    Add gitolite repo link shell script

---
+++ create_repo_link.sh
@@ -0,0 +1,99 @@
+ln -s /home/devops/workspace/webrtc-cache/boringssl.googlesource.com-boringssl /home/devops/repositories/boringssl.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-catapult /home/devops/repositories/catapult.git
+
+mkdir -p /home/devops/repositories/chromium/deps
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-deps-icu /home/devops/repositories/chromium/deps/icu.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-deps-libjpeg_turbo /home/devops/repositories/chromium/deps/libjpeg_turbo.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-deps-libsrtp /home/devops/repositories/chromium/deps/libsrtp.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-deps-nasm /home/devops/repositories/chromium/deps/nasm.git
+
+mkdir -p chromium/llvm-project/cfe/tools
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-llvm--project-cfe-tools-clang--format/ /home/devops/repositories/chromium/llvm-project/cfe/tools/clang-format.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-llvm--project-libcxx /home/devops/repositories/chromium/llvm-project/libcxx.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-llvm--project-libcxxabi /home/devops/repositories/chromium/llvm-project/libcxxabi.git
+
+mkdir -p /home/devops/repositories/chromium/src/
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-src-base /home/devops/repositories/chromium/src/base.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-src-build /home/devops/repositories/chromium/src/build.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-src-buildtools /home/devops/repositories/chromium/src/buildtools.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-src-testing /home/devops/repositories/chromium/src/testing.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-src-third_party /home/devops/repositories/chromium/src/third_party.git
+
+mkdir -p /home/devops/repositories/chromium/src/third_party
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-src-third_party-freetype2/ /home/devops/repositories/chromium/src/third_party/freetype2.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-src-tools/ /home/devops/repositories/chromium/src/tools.git
+
+mkdir -p /home/devops/repositories/chromium/third_party
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-third_party-ffmpeg/ /home/devops/repositories/chromium/third_party/ffmpeg.git
+
+mkdir -p /home/devops/repositories/chromium/tools
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-tools-depot_tools/ /home/devops/repositories/chromium/tools/depot_tools.git
+
+mkdir -p /home/devops/repositories/external
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-colorama/ /home/devops/repositories/external/colorama.git
+
+mkdir -p /home/devops/repositories/external/github.com/cisco
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-github.com-cisco-openh264/ /home/devops/repositories/external/github.com/cisco/openh264.git
+
+mkdir -p /home/devops/repositories/external/github.com/google
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-github.com-google-googletest/ /home/devops/repositories/external/github.com/google/googletest.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-github.com-google-gtest--parallel/ /home/devops/repositories/external/github.com/google/gtest-parallel.git
+
+mkdir -p /home/devops/repositories/external/github.com/harfbuzz
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-github.com-harfbuzz-harfbuzz/ /home/devops/repositories/external/github.com/harfbuzz/harfbuzz.git
+
+mkdir -p /home/devops/repositories/external/github.com/open-source-parsers
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-github.com-open--source--parsers-jsoncpp/ /home/devops/repositories/external/github.com/open-source-parsers/jsoncpp.git
+
+mkdir -p /home/devops/repositories/external/github.com/sctplab
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-github.com-sctplab-usrsctp/ /home/devops/repositories/external/github.com/sctplab/usrsctp.git
+
+mkdir -p /home/devops/repositories/external/llvm.org
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-external-llvm.org-libunwind/ /home/devops/repositories/external/llvm.org/libunwind.git
+
+mkdir -p /home/devops/repositories/infra/luci
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-infra-luci-client--py/ /home/devops/repositories/infra/luci/client-py.git
+
+mkdir -p /home/devops/repositories/libyuv
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-libyuv-libyuv/ /home/devops/repositories/libyuv/libyuv.git
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-linux--syscall--support/ /home/devops/repositories/linux-syscall-support.git
+
+mkdir -p /home/devops/repositories/webm
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-webm-libvpx/ /home/devops/repositories/webm/libvpx.git
+
+mkdir -p /home/devops/repositories/chromium/llvm-project/compiler-rt/lib
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-llvm--project-compiler--rt-lib-fuzzer/ /home/devops/repositories/chromium/llvm-project/compiler-rt/lib/fuzzer.git
+
+mkdir -p /home/devops/repositories/chromium/deps/yasm
+
+ln -s /home/devops/workspace/webrtc-cache/chromium.googlesource.com-chromium-deps-yasm-patched--yasm/ /home/devops/repositories/chromium/deps/yasm/patched-yasm.git
